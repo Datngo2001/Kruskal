d30b0fca4fb976a10254153947b2c07e676426e7
Ngô Đạt <supodatngo123@gmail.com>
2020-12-18 09:05:47 +0700

    Add project files.

---
+++ Kruskal/Graph.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Kruskal
+{
+    class Graph
+    {
+        protected List<Vertex> vertices = new List<Vertex>();
+        protected List<Edge> edges = new List<Edge>();
+        protected List<List<int>> Matrix = new List<List<int>>();
+        public List<Vertex> Vertices
+        {
+            get { return vertices; }
+            set { vertices = value; }
+        }
+        public List<Edge> Edges
+        {
+            get { return edges; }
+            set { edges = value; }
+        }
+        public Graph()
+        {
+
+        }
+        public Graph(List<Edge> edge, List<Vertex> vertice)
+        {
+            Edges = edge;
+            Vertices = vertice;
+        }
+        public Graph(List<List<int>> matrix)
+        {
+            Matrix = matrix;
+        }
+        public Graph(string pathMatrix)
+        {
+            ReadMatrix(pathMatrix);
+        }
+        protected void ReadMatrix(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] node = line.Split(" ");
+                for (int j = i; j < node.Length; j++)
+                {
+                    if(node[j] != "0")
+                    {
+                        int weight = Convert.ToInt32(node[j]);
+                        Edge edge = new Edge(i.ToString(), j.ToString(), weight);
+                        Edges.Add(edge);
+                    }
+                }
+                Vertex vertex = new Vertex((i + 1).ToString());
+                Vertices.Add(vertex);
+                i++;
+            }
+            reader.Close();
+        }
+    }
+}
---
+++ Kruskal/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kruskal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(@"C:\Users\supod\source\repos\Kruskal\Kruskal\matrix.txt");
+            Console.WriteLine("Thuat toan kruskal: ");
+            kruskal(graph);
+
+        }
+        public static void kruskal(Graph G)
+        {
+            List<Edge> F = new List<Edge>();
+
+            // 1. xap sep theo trong so tang dan
+            for (int i = 0; i < G.Edges.Count; i++)
+            {
+                for (int j = 0; j < G.Edges.Count - 1; j++)
+                {
+                    if(G.Edges[j].Weight > G.Edges[j + 1].Weight)
+                    {
+                        Edge a = G.Edges[j];
+                        G.Edges[j] = G.Edges[j + 1];
+                        G.Edges[j + 1] = a;
+                    }
+                }
+            }
+
+            // 2. them cac canh vao F neu khong tao nen chu trinh
+            // 3. lap lai den khi co n - 1 canh dc chon
+            //F.Add(G.Edges[0]);
+            while(F.Count < G.Vertices.Count - 1)
+            {
+                //xet tung canh, cac canh co duy nhat 1 nut trung voi cac nut cua cac canh da co trong F, 
+                //thi nhung canh do dc them vao F
+                for (int i = 0; i < G.Edges.Count; i++)
+                {
+                    bool add = true;
+                    for (int j = 0; j < F.Count; j++)
+                    {
+                        if(G.Edges[i].End1.Name == F[j].End1.Name || G.Edges[i].End1.Name == F[j].End2.Name)
+                        {
+                            for (int k = 0; k < F.Count; k++)
+                            {
+                                if (G.Edges[i].End2.Name == F[k].End2.Name || G.Edges[i].End2.Name == F[k].End1.Name)
+                                {
+                                    add = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    add = true;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    if (add == true)
+                    {
+                        F.Add(G.Edges[i]);
+                    }
+                }
+            }
+            // Show ket qua
+            for (int i = 0; i < F.Count; i++)
+            {
+                Console.WriteLine("(" + F[i].End1.Name + "," + F[i].End2.Name + ")" + " weight: " + F[i].Weight);
+            }
+
+        }
+    }
+}
